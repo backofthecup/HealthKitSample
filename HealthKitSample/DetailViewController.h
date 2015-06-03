3d86a360f960901808551a882f2ce490d074d18a
Eric <backofthecup@gmail.com>
2015-06-03 13:10:31 -0500

    Initial Commit

---
+++ HealthKitSample/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  HealthKitSample
+//
+//  Created by Eric Mansfield on 6/3/15.
+//  Copyright (c) 2015 Eric Mansfield. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ HealthKitSample/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  HealthKitSample
+//
+//  Created by Eric Mansfield on 6/3/15.
+//  Copyright (c) 2015 Eric Mansfield. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
